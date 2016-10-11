ef1e8ecef70cd428a2a39cae82855a2808459ee3
reymark <rey@leetdigital.com>
2016-10-11 09:14:58 +0800

    competed

---
+++ UiLab/UiLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UiLab
+//
+//  Created by Leet Digital Mac 15 #3 on 10/10/2016.
+//  Copyright © 2016 Leet Digital Mac 15 #3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
