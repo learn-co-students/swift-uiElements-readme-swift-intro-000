fba722b6663bc172e03cec808a77cb34348f4717
pacosta613 <pedro.acosta613@gmail.com>
2016-11-11 13:11:45 -0500

    Created new Xcode project with its elements

---
+++ First-Project-with-UIKIT/First Project with UIKIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First Project with UIKIT
+//
+//  Created by goat on 11/11/16.
+//  Copyright © 2016 goat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
