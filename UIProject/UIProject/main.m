42dbddde07e3229ab06a6ad2af71f295b641eabb
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-03-28 17:56:13 +1300

    ui lab

---
+++ UIProject/UIProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIProject
+//
+//  Created by Morgan McCauley on 28/03/17.
+//  Copyright © 2017 Morgan McCauley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
