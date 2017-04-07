26d89e710e2688c1084aca84bbb9f10dc328893b
kirillsh93 <kirill.shapoval@gmail.com>
2017-04-07 17:24:15 +0300

    UIKit

---
+++ UIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by Kirill Shapoval on 4/7/17.
+//  Copyright © 2017 Kirill Shapoval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
