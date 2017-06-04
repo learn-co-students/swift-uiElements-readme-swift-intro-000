2ca570b8db0593abbdd5233f13597181e2baab83
Garett Arrowood <garettarrowood@gmail.com>
2017-06-04 11:01:07 -0400

    Complete

---
+++ UIKitLearning/UIKitLearningTests/UIKitLearningTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIKitLearningTests.swift
+//  UIKitLearningTests
+//
+//  Created by Garett Arrowood on 6/4/17.
+//  Copyright © 2017 Garett Arrowood. All rights reserved.
+//
+
+import XCTest
+@testable import UIKitLearning
+
+class UIKitLearningTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
