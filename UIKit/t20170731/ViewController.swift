a7d888a51890193c8fde039cc7a02311743a8d1a
Whoopinstick <k.m.paul68@gmail.com>
2017-07-31 16:48:54 -0400

    add project

---
+++ UIKit/t20170731/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  t20170731
+//
+//  Created by Kevin Paul on 7/31/17.
+//  Copyright © 2017 Whoopinstick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
