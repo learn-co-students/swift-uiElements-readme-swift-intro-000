16bfb4e39cf8422d05b9b08e859338b3de29543d
Trevonte <trevontedixon@gmail.com>
2017-11-09 18:45:12 -0500

    UIKit lab solution

---
+++ UIKitLab/UIKitLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKitLab
+//
+//  Created by Trevonte Dixon on 11/9/17.
+//  Copyright © 2017 1umbrella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
