7100fed45301b69774c19706e7a00fe288bde249
shanetreynolds6 <shanetreynolds@icloud.com>
2017-11-13 21:52:51 -0500

    UIKit Elements Lab

---
+++ UIKit Elements Lab.swift
@@ -0,0 +1,9 @@
+//
+//  UIKit Elements Lab.swift
+//  ArrayChallengeDeuce2
+//
+//  Created by Shane Reynolds on 11/13/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
---
+++ UIKit Elements Lab/UIKit Elements Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKit Elements Lab
+//
+//  Created by Shane Reynolds on 11/13/17.
+//  Copyright © 2017 Shane Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
