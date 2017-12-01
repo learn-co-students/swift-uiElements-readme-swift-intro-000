9e788fd433c79c1bb28e4dfdc7b6e4a329b60d35
Alex <onischenko_ay@inbox.ru>
2017-12-01 18:55:13 +0300

    done

---
+++ testUI/TestUI/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestUI
+//
+//  Created by Alexander Onishchenko on 01/12/2017.
+//  Copyright © 2017 Alexander Onishchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
