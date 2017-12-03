a4409d92845cca66cc7306d345559f58ecdcefb5
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2017-12-02 22:37:37 -0600

    Add files via upload

---
+++ uiTest/uiTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  uiTest
+//
+//  Created by Luis Romo on 02/12/17.
+//  Copyright © 2017 Luis Romo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var stepperBtn: UIStepper!
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
