4f9bc759ddd82bf81315c0346deba6246ffad315
nikandv <nikandrov.ant@ya.ru>
2018-02-13 15:34:33 +0300

    Complete

---
+++ UiElements1/UiElements1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UiElements1
+//
+//  Created by Антон Никандров on 13.02.2018.
+//  Copyright © 2018 Anton Nikandrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
