25b82d0c0ad6fc7773d37082b1747de8a349e547
flanding001 <frederik@landing.se>
2018-02-28 20:30:24 +0100

    Complete problems

---
+++ TestUI/TestUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestUI
+//
+//  Created by Frederik Landing on 2018-02-28.
+//  Copyright © 2018 Landing IT-Konsult AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
