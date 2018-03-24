457dc2f4983ad4884f2dd76385deff815ac89c89
sheziasn <sheziasn1@hotmail.com>
2018-03-24 11:47:08 +0300

    done

---
+++ UIKit001/UIKit001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKit001
+//
+//  Created by Shehzad Shaukat on 243//18.
+//  Copyright © 2018 Shehzad Shaukat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
