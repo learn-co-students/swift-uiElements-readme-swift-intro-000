7b1258675df0aaad1cdedf30fc42381ebef3d45d
bodeacorina <corina.bodea@yahoo.com>
2018-09-25 11:41:14 +0300

    Add UIKit solution

---
+++ UIProject/UIProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIProject
+//
+//  Created by Corina Bodea on 25/09/2018.
+//  Copyright © 2018 Corina Bodea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
