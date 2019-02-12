65a37d8d49cebb724b9a52f4ec17145aa6bee0a2
pmaurelia <pmaurelia@gmail.com>
2019-02-11 22:33:24 -0500

    add most of the regular UI controls

---
+++ New UI Controls/New UI Controls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New UI Controls
+//
+//  Created by Pablo Maurelia on 2/11/19.
+//  Copyright © 2019 Pablo Maurelia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
